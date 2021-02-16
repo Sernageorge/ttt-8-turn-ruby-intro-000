0e1cb17e92fc49611d4a02eb44c5a1495a82ffd3
George Serna <sernageorge@gmail.com>
2021-02-16 23:45:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,4 @@
+# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,34 +8,29 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input.to_i-1
+  user_input.to_i - 1
 end
 
-def move(board, index, token="X")
-  board[index] = token
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
 
-def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O"
-    true
-  else
-    false
-  end
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
 end
 
 def valid_move?(board, index)
-  !position_taken?(board, index) && index.between?(0, 8)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  move(board, index)
-  valid_move?(board, index)
-  if !index.between?(0, 8)
-    return turn(board)
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
   else
-    return display_board(board)
+    turn(board)
   end
 end
